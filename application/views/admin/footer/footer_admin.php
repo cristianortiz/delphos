878143019edd42c4f4fa28428c80bbab093639d0
cristian ortiz <kristianortiz300@hotmail.com>
2012-05-08 00:26:57 -0400

    ootra actualizacion

--- application/views/admin/footer/footer_admin.php
+++ application/views/admin/footer/footer_admin.php
@@ -1,8 +1,6 @@
 <div id="contenido-footer">
-              
-                    <div id="footer_admin" style="margin-top: 20px;">
-                    <h3>Sitio Desarrollado por el Departamento Ingenieria Informatica y Ciencias de la Computacion</h3><br />
-                    <h4>DIICC - UDA - 2011</h4>
-                        
-                    </div>                                                             
+  <div id="footer_admin" style="margin-top: 20px;">
+    <h3>Sitio Desarrollado por el Departamento Ingenieria Informatica y Ciencias de la Computacion</h3><br />
+     <h4>DIICC - UDA - 2011</h4>                        
+  </div>                                                             
 </div>            
