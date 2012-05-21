d5a5f7727f6d93a5d0b54d44fe0b913f1e05a176
jorgito <cristian.ortiz@alumnos.uda.cl>
2012-05-21 19:31:07 -0400

    otros cambios a script.js

--- recursos/js/script.js
+++ recursos/js/script.js
@@ -111,7 +111,22 @@ $("#lateral").carouFredSel({
            
          /*fin content-rotator panel principal
         ---------------------------------------------------------------------------------------------------------------------*/
-  
+  /*-------------------------------------------------------------------------------------------------------------
+    Bloque JS para el plugin projekktor, llama al metodo carga_playlist delcontrolador home que carga la lista de
+    reproduccion desde la BD en formato JSON para el reproductor
+  ----------------------------------------------------------------------------------------------------------------*/ 
+   projekktor('#player_a', {
+	debug: false,
+      // poster: 'intro.png',
+      useYTIframeAPI: false, 
+	width: 980,
+	height: 570,
+    playerFlashMP4:         base_url+'/recursos/js/jarisplayer.swf',
+    playerFlashMP4:         base_url+'recursos/js/jarisplayer.swf',
+
+	controls: true,
+	playlist: [{0:{src:base_url+'/home/carga_playlist', type:"text/json"}}] 
+    });  
   
   
 });
